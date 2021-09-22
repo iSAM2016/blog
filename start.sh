8731e2d5be361c9ee62afd00aa481428974825fc
isam2016 <isam2016@qq.com>
2021-09-22 16:06:24 +0800

    修改脚本

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 # ### hexo s
 
 # push code and run 
-ssh -tt root@192.168.8.164 "cd /home/isam2016/iSAM2016.github.io; git pull; hexo g "
+ssh -tt root@192.168.8.164 "cd /home/isam2016/blog; git pull; hexo g "
